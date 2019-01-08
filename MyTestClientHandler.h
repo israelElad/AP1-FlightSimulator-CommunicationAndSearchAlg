f596b6174b1a84ce6d08eb1a23cce555b06644af
Narkis Shallev <karmizi101@gmail.com>
2019-01-08 18:00:26 +0200

    Main

--- MyTestClientHandler.h
+++ MyTestClientHandler.h
@@ -8,6 +8,8 @@
 #include "Solver.h"
 #include <iostream>
 #include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
 
 template<typename P, typename S>
 class MyTestClientHandler :public ClientHandler{
@@ -18,22 +20,61 @@ public:
         this->solver = solver;
         this->cacheManager = cacheManager;
     }
-    virtual void handleClient(istream istream1, ostream ostream1) {
-        char buffer[256];
-        istream1.getline(buffer, 256);
-        while (strcmp(buffer, "end") != 0) {
+    virtual void handleClient(int socketFd) {
+        string buffer=readLineFromSocket(socketFd);
+
+
+        while (strcmp(buffer.c_str(), "end") != 0) {
             bool checkResult = this->cacheManager.isSaved(buffer);
             if (checkResult) {
-                S solution = this->cacheManager.getSolution(buffer);
-                ostream1 << solution;
+                //converts solution to string in order to send it to the client
+                string solution = this->cacheManager.getSolution(buffer);
+                send(socketFd, solution.c_str(), solution.length(), 0);
             } else {
                 S solution = this->solver.solve(buffer);
                 this->cacheManager.saveSolution(buffer, solution);
-                ostream1 << solution;
             }
-            flush(ostream1);
-            istream1.getline(buffer, 256);
+//        fflush(ostream1); todo
+            buffer=readLineFromSocket(socketFd);
         }
+    }
+
+    string MyTestClientHandler<P, S>::readLineFromSocket(int socketFd) {
+        //todo: improve if there is time
+//    std::string line;
+//    char buf[1024];
+//    int n = 0;
+//        while(n = read(newSockFd, buf, 1024))
+//        {
+//            size_t pos = std::find(buf, buf + n, '\n');
+//            if(pos != std::string::npos)
+//            {
+//                if (pos < 1024-1 && buf[pos + 1] == '\n')
+//                    break;
+//            }
+//            line += buf;
+//        }
+//
+//        line += buf;
+
+        //read line into buffer
+        char buffer[1024];
+        int n = 0;
+        char c = 'n';
+        int idx = 0;
+        while (c != '\n')   {
+            n = static_cast<int>(read(socketFd, &c, 1));
+            if (n < 0) {
+                perror("ERROR reading from socket");
+                return nullptr;
+            }   else if (n == 0)    {
+                perror("Socket is closed");
+                return nullptr;
+            }
+            buffer[idx++] = c;
+        }
+        buffer[idx] = 0;
+        return buffer;
     }
 };
 
